cf70716833ba3ddef2415ae6d42d52b8f79c0e5a
kevinesc21 <00116120@uca.edu.sv>
2022-07-02 16:06:28 -0600

    Eliminar proyecto innecesario

--- ThreeFragmentsActivity/app/src/main/java/com/escobar/threefragmentsactivity/MainViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.escobar.threefragmentsactivity
-
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-
-class MainViewModel : ViewModel() {
-    val meatsQuantity = MutableLiveData("")
-    val complement = MutableLiveData("")
-}
